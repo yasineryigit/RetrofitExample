e0a4980494da458224363cb6c7b8c4fbb15a9591
yasineryigit <yasineryigit737@gmail.com>
2021-03-23 18:31:53 +0300

    URL Manipulation added

--- app/src/main/java/com/ossovita/retrofitexample/MainActivity.java
+++ app/src/main/java/com/ossovita/retrofitexample/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ossovita.retrofitexample.api.JsonPlaceHolderApi;
+import com.ossovita.retrofitexample.model.Comment;
 import com.ossovita.retrofitexample.model.Post;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,6 +25,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +39,57 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        
+        getPosts();
+        //getComments();
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = jsonPlaceHolderApi
+                .getComments(2);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Error: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return; //hata varsa daha fazla reponse metodunda bekleme
+                }
+                List<Comment> comments = response.body();
+                for(Comment comment: comments){
+
+                    String content="";
+                    content += "ID: " + comment.getId() + "\n";
+                    content += "Post ID: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName();
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Text: " + comment.getText() + "\n\n";
+                    textViewResult.append(content);
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getPosts() {
+        Map<String,String> parameters = new HashMap<>();
+        parameters.put("userId","1");
+        parameters.put("_sort","id");
+        parameters.put("_order","desc");
+        ArrayList<Integer> userIdList = new ArrayList<>();
+        userIdList.add(2);
+        userIdList.add(5);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(userIdList);//bu parametrelere göre veri getir
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -64,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
                 textViewResult.setText(t.getMessage());
             }
         });
-
 
     }
 }
